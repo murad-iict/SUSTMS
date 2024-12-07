aa3578edb3d48de0203b14ea7859f3326969081a
murad <murad02@student.sust.edu>
2024-12-08 01:45:49 +0600

    second commit

--- SUSTMS/urls.py
+++ SUSTMS/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('CL.urls')),
 ]
+
+#admin.site.index_title = "Accounts Management System for IICT, SUST"
+#admin.site.site_header = "AMSI Admin Panel"
+#admin.site.site_title = "AMSI"
